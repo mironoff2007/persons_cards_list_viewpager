ebd8252dd4378645e25aef596c0d09c69cb1039a
mironoff2007 <mironoff2007@rambler.ru>
2022-01-16 22:56:13 +0300

    Change MutableLiveData to MutableStateFlow

--- app/src/main/java/ru/mironov/persons_cards_list_viewpager/Repository.kt
+++ app/src/main/java/ru/mironov/persons_cards_list_viewpager/Repository.kt
@@ -1,6 +1,6 @@
 package ru.mironov.persons_cards_list_viewpager
 
-import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.flow.MutableStateFlow
 import retrofit2.Call
 import ru.mironov.persons_cards_list_viewpager.retrofit.JsonArrayUsers
 import ru.mironov.persons_cards_list_viewpager.retrofit.JsonUser
@@ -11,7 +11,7 @@ open class Repository @Inject constructor(protected val retrofit: UsersApi) {
 
     var usersList:ArrayList<JsonUser?>?=null
 
-    var mutableSearchParam = MutableLiveData<SortParams>()
+    var mutableSearchParam = MutableStateFlow<SortParams?>(null)
 
     fun getUsers(): Call<JsonArrayUsers> {
         return retrofit.getUsers()
--- app/src/main/java/ru/mironov/persons_cards_list_viewpager/viewmodel/UsersListFragmentViewModel.kt
+++ app/src/main/java/ru/mironov/persons_cards_list_viewpager/viewmodel/UsersListFragmentViewModel.kt
@@ -1,14 +1,13 @@
 package ru.mironov.persons_cards_list_viewpager.viewmodel
 
-import android.os.Build
-import android.widget.Toast
-import androidx.annotation.RequiresApi
+
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import ru.mironov.persons_cards_list_viewpager.Repository
 import ru.mironov.persons_cards_list_viewpager.SortBy
@@ -19,20 +18,18 @@ import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
-class UsersListFragmentViewModel @Inject constructor(protected val repository: Repository) : ViewModel() {
+class UsersListFragmentViewModel @Inject constructor(protected val repository: Repository) :
+    ViewModel() {
 
     lateinit var allDepartmentName: String
 
     var mutableStatus = MutableLiveData<Status>()
 
-    var observer: Observer? = null
-
-
     fun getParams(): SortParams? {
         return repository.mutableSearchParam.value
     }
 
-    private fun getUsersWithSort(department: String,params:SortParams) {
+    private fun getUsersWithSort(department: String, params: SortParams) {
 
         val list = repository.usersList
 
@@ -44,18 +41,17 @@ class UsersListFragmentViewModel @Inject constructor(protected val repository: R
             status.usersList =
                 list?.filter { user ->
                     (user?.department == department ||
-                            department == allDepartmentName )&&
+                            department == allDepartmentName) &&
                             (user?.lastName!!.lowercase().contains(params.searchBy) ||
-                            user?.firstName!!.lowercase().contains(params.searchBy) ||
-                            user?.userTag!!.lowercase().contains(params.searchBy) ||
-                            params.searchBy.isEmpty())
+                                    user.firstName!!.lowercase().contains(params.searchBy) ||
+                                    user.userTag!!.lowercase().contains(params.searchBy) ||
+                                    params.searchBy.isEmpty())
                 } as ArrayList<JsonUser?>?
 
             //Sort
-            if(params.sortBy==SortBy.ALPHABET_SORT){
+            if (params.sortBy == SortBy.ALPHABET_SORT) {
                 status.usersList?.sortBy { it?.firstName }
-            }
-            else{
+            } else {
                 status.usersList?.sortBy { it?.birthday }
             }
 
@@ -63,11 +59,11 @@ class UsersListFragmentViewModel @Inject constructor(protected val repository: R
         }
     }
 
-
-    fun listenSearchParam(department:String) {
-        repository.mutableSearchParam.observeForever() { params ->
-            getUsersWithSort(department,params)
-        }
+    fun listenSearchParam(department: String) {
+        repository.mutableSearchParam.onEach { params ->
+            if (params != null) {
+                getUsersWithSort(department, params)
+            }
+        }.launchIn(viewModelScope)
     }
-
 }
