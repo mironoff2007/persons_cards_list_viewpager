e2250a25261c5ba90856d73bca0b82b64b31cdd1
mironoff2007 <mironoff2007@rambler.ru>
2022-01-17 23:53:58 +0300

    Header is added for dynamic data

--- app/src/main/java/ru/mironov/persons_cards_list_viewpager/retrofit/UsersApi.kt
+++ app/src/main/java/ru/mironov/persons_cards_list_viewpager/retrofit/UsersApi.kt
@@ -1,13 +1,14 @@
 package ru.mironov.persons_cards_list_viewpager.retrofit
 
-
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
 
 interface UsersApi {
 
+    @Headers("Prefer: code=200, dynamic=true")
     @GET("users")
-    fun getUsers(
-    ): Call<JsonArrayUsers>
+    fun getUsers(): Call<JsonArrayUsers>
 
 }
