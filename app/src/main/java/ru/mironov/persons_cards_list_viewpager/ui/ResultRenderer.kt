192e5c4e6ef743dfd402640b02829e71dc99133f
mironoff2007 <mironoff2007@rambler.ru>
2022-01-17 23:53:36 +0300

    Merge part_result layout

--- app/src/main/java/ru/mironov/persons_cards_list_viewpager/ui/ResultRenderer.kt
+++ app/src/main/java/ru/mironov/persons_cards_list_viewpager/ui/ResultRenderer.kt
@@ -2,13 +2,10 @@ package ru.mironov.persons_cards_list_viewpager.ui
 
 import android.view.View
 import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
 import kotlinx.android.synthetic.main.part_result.view.*
 import ru.mironov.persons_cards_list_viewpager.R
 import ru.mironov.persons_cards_list_viewpager.Status
-import ru.mironov.persons_cards_list_viewpager.retrofit.JsonUser
-import java.util.ArrayList
 
 object ResultRenderer {
     fun renderResult(status: Status, partResult: LinearLayout) {
@@ -18,35 +15,36 @@ object ResultRenderer {
                 partResult.visibility= View.GONE
             }
             is Status.LOADING -> {
+                partResult.progressBar.visibility= View.VISIBLE
                 partResult.visibility= View.VISIBLE
-                partResult.result_image.visibility= View.GONE
-                partResult.result_text_top.visibility= View.GONE
-                partResult.result_text_middle.visibility= View.GONE
-                partResult.result_text_bottom.visibility= View.GONE
+                partResult.resultImage.visibility= View.GONE
+                partResult.resultTextTop.visibility= View.GONE
+                partResult.resultTextMiddle.visibility= View.GONE
+                partResult.resultTextBottom.visibility= View.GONE
             }
             is Status.ERROR -> {
                 partResult.visibility= View.VISIBLE
-                partResult.result_text_top.visibility= View.VISIBLE
-                partResult.result_text_middle.visibility= View.VISIBLE
-                partResult.result_text_bottom.visibility= View.VISIBLE
-                partResult.result_image.visibility= View.VISIBLE
+                partResult.resultTextTop.visibility= View.VISIBLE
+                partResult.resultTextMiddle.visibility= View.VISIBLE
+                partResult.resultTextBottom.visibility= View.VISIBLE
+                partResult.resultImage.visibility= View.VISIBLE
                 partResult.progressBar.visibility= View.GONE
-                partResult.result_image.setImageDrawable(AppCompatResources.getDrawable(partResult.context,R.drawable.ufo))
-                partResult.result_text_top.setText(R.string.someone_broke_everything)
-                partResult.result_text_middle.setText(R.string.we_will_fix)
-                partResult.result_text_bottom.setText(R.string.try_again)
+                partResult.resultImage.setImageDrawable(AppCompatResources.getDrawable(partResult.context,R.drawable.ufo))
+                partResult.resultTextTop.setText(R.string.someone_broke_everything)
+                partResult.resultTextMiddle.setText(R.string.we_will_fix)
+                partResult.resultTextBottom.setText(R.string.try_again)
             }
             is Status.EMPTY -> {
                 partResult.visibility= View.VISIBLE
-                partResult.result_image.visibility= View.VISIBLE
-                partResult.result_text_top.visibility= View.VISIBLE
-                partResult.result_text_middle.visibility= View.VISIBLE
-                partResult.result_text_bottom.visibility= View.VISIBLE
+                partResult.resultImage.visibility= View.VISIBLE
+                partResult.resultTextTop.visibility= View.VISIBLE
+                partResult.resultTextMiddle.visibility= View.VISIBLE
+                partResult.resultTextBottom.visibility= View.VISIBLE
                 partResult.progressBar.visibility= View.GONE
-                partResult.result_image.setImageDrawable(AppCompatResources.getDrawable(partResult.context,R.drawable.search_glass))
-                partResult.result_text_top.setText(R.string.nobody_found)
-                partResult.result_text_middle.setText(R.string.correct_your_search)
-                partResult.result_text_bottom.setText(R.string.empty)
+                partResult.resultImage.setImageDrawable(AppCompatResources.getDrawable(partResult.context,R.drawable.search_glass))
+                partResult.resultTextTop.setText(R.string.nobody_found)
+                partResult.resultTextMiddle.setText(R.string.correct_your_search)
+                partResult.resultTextBottom.setText(R.string.empty)
             }
         }
     }
