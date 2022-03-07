1aa19969b8b3453f10ae0e50637d8e64ce7c6fdb
Mironov Ury <mironoff2007@rambler.ru>
2022-03-07 22:21:23 +0300

    sleep in test is changed to CountDownLatch

--- app/src/androidTest/java/ru/mironov/persons_cards_list_viewpager/UserListViewModelTest.kt
+++ app/src/androidTest/java/ru/mironov/persons_cards_list_viewpager/UserListViewModelTest.kt
@@ -8,21 +8,24 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.*
-import kotlinx.coroutines.test.TestCoroutineScope
 import org.junit.After
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
 import ru.mironov.persons_cards_list_viewpager.data.Repository
+import ru.mironov.persons_cards_list_viewpager.data.retrofit.UsersApi
+import ru.mironov.persons_cards_list_viewpager.domain.JsonUser
 import ru.mironov.persons_cards_list_viewpager.domain.SortBy
 import ru.mironov.persons_cards_list_viewpager.domain.SortParams
-import ru.mironov.persons_cards_list_viewpager.domain.JsonUser
-import ru.mironov.persons_cards_list_viewpager.data.retrofit.UsersApi
 import ru.mironov.persons_cards_list_viewpager.domain.Status
 import ru.mironov.persons_cards_list_viewpager.presentation.viewmodel.UsersListFragmentViewModel
 import java.lang.Thread.sleep
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 @RunWith(AndroidJUnit4::class)
 class UserListViewModelTest {
@@ -35,6 +38,14 @@ class UserListViewModelTest {
     private lateinit var observer: Observer<Status>
 
     private lateinit var context: Context
+
+    companion object{
+        const val TIME_STEP=100L
+        const val ERROR_MESSAGE="LiveData value was never set."
+        val timeUnit = TimeUnit.SECONDS
+    }
+
+    private var latch = CountDownLatch(1)
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -52,8 +63,11 @@ class UserListViewModelTest {
 
         viewModelUserList = UsersListFragmentViewModel(repository)
         viewModelUserList.allDepartmentName = context.getString(R.string.department_all)
+
         setupObserver()
+        latch=CountDownLatch(1)
     }
+
 
     /**
      * После того как вынес поиск в отдельную функцию
@@ -63,22 +77,29 @@ class UserListViewModelTest {
 
     @Test
     fun checkListSize() {
+
         runBlocking {
             viewModelUserList.listenSearchParam(viewModelUserList.allDepartmentName)
             repository.mutableSearchParam.value = SortParams("", SortBy.ALPHABET_SORT)
         }
-        sleep(100)
+        //Wait
+        if (!latch.await(TIME_STEP, timeUnit)) { throw TimeoutException(ERROR_MESSAGE) }
+
         assert(resultData.usersList!!.size==80)
     }
 
 
     @Test
     fun testSearchByNameAndDepartment() {
+
         runBlocking {
             viewModelUserList.listenSearchParam(context.getString(R.string.department_android))
             repository.mutableSearchParam.value = SortParams("Hanna", SortBy.ALPHABET_SORT)
         }
-        sleep(100)
+
+        //Wait
+        if (!latch.await(TIME_STEP, timeUnit)) { throw TimeoutException(ERROR_MESSAGE) }
+
         val lastName = resultData.usersList!![0]!!.lastName
         assert(resultData.usersList!!.size == 1 && lastName == "Kling")
     }
@@ -89,7 +110,10 @@ class UserListViewModelTest {
             viewModelUserList.listenSearchParam(viewModelUserList.allDepartmentName)
             repository.mutableSearchParam.value = SortParams("not_match", SortBy.ALPHABET_SORT)
         }
-        sleep(100)
+
+        //Wait
+        if (!latch.await(TIME_STEP, timeUnit)) { throw TimeoutException(ERROR_MESSAGE) }
+
         assert(resultStatus == Status.EMPTY)
     }
 
@@ -99,17 +123,29 @@ class UserListViewModelTest {
             viewModelUserList.listenSearchParam(context.getString(R.string.department_android))
             repository.mutableSearchParam.value = SortParams("", SortBy.ALPHABET_SORT)
         }
-    sleep(100)
+
+        //Wait
+        if (!latch.await(TIME_STEP, timeUnit)) { throw TimeoutException(ERROR_MESSAGE) }
+
         assert(resultData.usersList!!.size==10)
     }
 
     private fun setupObserver() {
         observer = Observer<Status> { status ->
             when (status) {
-                is Status.DATA -> { resultData = status }
+                is Status.DATA -> {
+                    resultData = status
+                    latch.countDown()
+                }
                 is Status.LOADING -> {}
-                is Status.ERROR -> { resultStatus = status }
-                is Status.EMPTY -> { resultStatus = status }
+                is Status.ERROR -> {
+                    resultStatus = status
+                    latch.countDown()
+                }
+                is Status.EMPTY -> {
+                    resultStatus = status
+                    latch.countDown()
+                }
             }
         }
         viewModelUserList.mutableStatus.observeForever(observer)
